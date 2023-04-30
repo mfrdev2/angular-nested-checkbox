f1b74a181da6812b9e8723772538ea36c210e468
FRabbi <mdfazlerabbi.diu@gmail.com>
2023-05-01 01:38:48 +0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,170 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  isUserPerformOnCB = false;
+
+  menuList = [
+    { id: 1, title: 'Dashboard', rawMenus: [
+      { id: 101, title: 'View Dashboard' },
+      { id: 102, title: 'Edit Dashboard' }
+    ]},
+    { id: 2, title: 'Users', rawMenus: [
+      { id: 101, title: 'View Dashboard' },
+      { id: 102, title: 'Edit Dashboard' }
+    ]},
+    { id: 3, title: 'Roles', rawMenus: [
+      { id: 101, title: 'View Dashboard' },
+      { id: 102, title: 'Edit Dashboard' }
+    ]}
+  ];
+
+  assaignMenuList = [
+    {
+       id: 1, title: 'Dashboard', rawMenus: [
+        { id: 101, title: 'View Dashboard' }
+      ]
+    },
+    { id: 2, title: 'Users', rawMenus: [
+      { id: 101, title: 'View Dashboard' },
+      { id: 102, title: 'Edit Dashboard' }
+    ]}
+  ]
+
+  userGroupPermisisonForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.userGroupPermisisonForm = this.formBuilder.group({
+      menuBeans: this.formBuilder.array(
+        this.menuList.map(item => this.formBuilder.group({
+          status: [false],
+          permissions: this.formBuilder.array(
+            item.rawMenus.map(rowMenu => this.formBuilder.group({
+              id: [rowMenu.id],
+              allowed: [false]
+            }))
+          )
+        }))
+      )
+    });
+
+    setTimeout(()=>{
+      console.log('patch data....')
+        this.patchMenu();
+    },500)
+  }
+
+  // setPermission(item: any, checked: boolean, rowMenu: any) {
+  //   const menuId = rowMenu.id;
+  //   for (let i = 0; i < this.menuList.length; i++) {
+  //     if (this.menuList[i].id === item.id) {
+  //       const permissions = this.userGroupPermisisonForm.get(`menuBeans.${i}.permissions`) as FormArray;
+  //       for (let j = 0; j < permissions.length; j++) {
+  //         const permission = permissions.at(j) as FormGroup;
+  //         if (permission.value.id === menuId) {
+  //           permission.patchValue({
+  //             allowed: checked
+  //           });
+  //           break;
+  //         }
+  //       }
+  //       break;
+  //     }
+  //   }
+  // }
+
+  setPermission(item: any, checked: boolean, rowMenu: any) {
+    this.isUserPerformOnCB = true;
+    console.log(item)
+    const menuId = rowMenu.id;
+
+        const permissions = this.userGroupPermisisonForm.get(`menuBeans.${item}.permissions`) as FormArray;
+        for (let j = 0; j < permissions.length; j++) {
+          const permission = permissions.at(j) as FormGroup;
+          if (permission.value.id === menuId) {
+            permission.patchValue({
+              allowed: checked
+            });
+            break;
+          }
+        }
+      
+    }
+
+
+  getCheckStatus(menuId: number): boolean {
+    for (let i = 0; i < this.menuList.length; i++) {
+      const item = this.userGroupPermisisonForm.get(`menuBeans.${i}`) as FormGroup;
+      if (item.value.status) {
+        const permissions = item.get('permissions') as FormArray;
+        for (let j = 0; j < permissions.length; j++) {
+          const permission = permissions.at(j) as FormGroup;
+          if (permission.value.id === menuId) {
+            return permission.value.allowed;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  isPermissionAllowed(item: any,menuIndex:number, rowMenu: any,rawMenuIndex:number): boolean {
+    const menuId = item.id;
+    const rawMenuId = rowMenu.id;
+   // console.log('menuId :',menuId,' rawMenuId ',rawMenuId,' result ::',this.isRawMenuPresent(menuId,menuIndex,rawMenuId,rawMenuIndex))
+    return this.isRawMenuPresent(menuId,menuIndex,rawMenuId,rawMenuIndex);
+  }
+
+  isRawMenuPresent(menuId: number,menuIndex:number, rawMenuId: number,rawMenuIndex:number ): boolean {
+
+    const permissions = this.userGroupPermisisonForm.get(`menuBeans.${menuIndex}.permissions`) as FormArray;
+
+    for (const menu of this.assaignMenuList) {
+      if (menu.id === menuId) {
+        for (const rawMenu of menu.rawMenus) {
+          if (rawMenu.id === rawMenuId) {
+            const permission = permissions.at(rawMenuIndex) as FormGroup;
+              permission.patchValue({
+                allowed: true
+              });
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  patchMenu(){
+    let menuBeans: { status: boolean; }[] = [];
+    this.menuList.forEach((data,index)=>{
+      if(this.isPresentAtAssaignData(data.id)){
+        menuBeans.push({
+          status: true
+        });
+      }else{
+        menuBeans.push({
+          status: false
+        });
+      }
+       
+    })
+
+       this.userGroupPermisisonForm.patchValue({menuBeans})
+  }
+
+  isPresentAtAssaignData(id:any):boolean{
+    for (const item of this.assaignMenuList) {
+      if(item.id === id){
+        return true;
+      }
+    }
+    return false;
+  }
+  onSubmit(){
+    console.log(this.userGroupPermisisonForm.get('menuBeans')?.value);
+  }
+
 }
---
+++ src/app/material.module.ts
@@ -0,0 +1,105 @@
+
+import {NgModule} from '@angular/core';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCommonModule, MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import { MatSortModule } from '@angular/material/sort';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
+@NgModule({
+  imports:[
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatCommonModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatTabsModule,
+    MatTooltipModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatRadioModule,
+    MatNativeDateModule,
+    MatSortModule,
+    MatProgressBarModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+
+  ],
+  exports: [
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatCommonModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatTabsModule,
+    MatTooltipModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatRadioModule,
+    MatNativeDateModule,
+    MatSortModule,
+    MatProgressBarModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+  ]
+})
+export class MaterialModule {
+}
